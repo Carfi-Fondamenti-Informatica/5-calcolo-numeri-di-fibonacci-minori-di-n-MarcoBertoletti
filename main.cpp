83dfb08ea9d367e23695a07ef24e6ae727f422ea
Marco Bertoletti <100340284+MarcoBertoletti@users.noreply.github.com>
2022-04-03 18:56:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   int x=1, y=0, z=0, n;
-   cin >>n;
-   while(z<=n){
-       z=x+y;
-       x=y;
-       y=z;
-       if(z<=n){
-           cout <<z <<endl;
-       }
-   }
-   return 0;
+int fibonacci (int z=0) {
+    int x=1, y=0, n=0;
+    cin >>n;
+    for (;z<=n;){
+        z=x+y;
+        x=y;
+        y=z;
+        if(z<=n){
+            cout <<z <<endl;
+        }
+    }
+    return z;
+}
+int main(){
+    fibonacci(0);
 }
